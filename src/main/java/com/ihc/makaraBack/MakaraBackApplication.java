f1b65e5a2b5739527e3cc5a9cfdb80205a5dc7ee
ireherchm <irehervasch@gmail.com>
2020-03-29 01:47:12 +0100

    Initial commit

---
+++ src/main/java/com/ihc/makaraBack/MakaraBackApplication.java
@@ -0,0 +1,12 @@
+package com.ihc.makaraBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakaraBackApplication {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(MakaraBackApplication.class);
+        app.run(args);
+    }
+}
